d80a710690fda0610ed5921af3574d66a6f2cf3a
carznot2 <g.shaw@shaw.ca>
2023-04-21 19:13:01 -0600

    resubmit

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+const date = new Date()
+document.querySelector("#year").textContent=date.getFullYear()
+
+console.log(date.getFullYear())
+document.querySelector("#lastUpdated").textContent=document.lastModified
